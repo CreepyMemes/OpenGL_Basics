cd1e9d32e84ed8e7df482168f977158fe54c683f
CreepyMemes <mjouins@gmail.com>
2023-05-24 21:49:40 +0200

    readded error handling files

---
+++ src/GFX/error_handling.h
@@ -0,0 +1,9 @@
+#include "gfx.h"
+
+#include <iostream>
+#include <string>
+
+// Macro that checks for error codes, if one is found prints in which file and line it was called
+#define glCheckError() getErrorCode(__FILE__, __LINE__) 
+
+GLenum getErrorCode(const char *file, int line);
