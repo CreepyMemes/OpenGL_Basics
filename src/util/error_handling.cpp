e1c93e526fe6b15cb53ef357a88b5fce6bb95c2e
CreepyMemes <mjouins@gmail.com>
2023-05-30 16:52:13 +0200

    moved error_handling to util folder

---
+++ src/util/error_handling.cpp
@@ -0,0 +1,23 @@
+#include "error_handling.h"
+
+// Check if there are errors and return False if an error is found (so assert can kill the program)
+GLenum getErrorCode(const char *file, int line)
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+            //case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+            //case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+        }
+        std::cerr << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
