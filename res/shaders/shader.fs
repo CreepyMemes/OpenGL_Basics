f2eef2360753b145cc40664f696a0026952f8362
CreepyMemes <mjouins@gmail.com>
2023-05-31 17:34:46 +0200

    cleaned shaders code

--- res/shaders/shader.fs
+++ res/shaders/shader.fs
@@ -1,13 +1,15 @@
 #version 330 core
 
-out vec4 FragColor;
-in vec3 ourColor;
-
+in vec3 v_aColor;
 in vec2 v_aTexture;
+
+out vec4 FragColor;
+
 uniform sampler2D u_Texture;
 
 void main(){
-    vec4 texColor = texture(u_Texture, v_aTexture);
-    vec4 colColor = vec4(ourColor, 1.0);
-    FragColor = texColor * colColor;
+    vec4 color_texture   = texture(u_Texture, v_aTexture);
+    vec4 color_attribute = vec4(v_aColor, 1.0);
+
+    FragColor = color_texture * color_attribute;
 }
--- res/shaders/shader.vs
+++ res/shaders/shader.vs
@@ -2,18 +2,16 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
-
 layout (location = 2) in vec2 aTexture;
-out vec2 v_aTexture;
 
-out vec3 ourColor;
+out vec3 v_aColor;
+out vec2 v_aTexture;
 
 uniform float xoffset;
 uniform float yoffset;
 
 void main(){
     gl_Position = vec4(aPos.x + xoffset, aPos.y + yoffset, aPos.z, 1.0);
-    ourColor = aColor;
-
-    v_aTexture = aTexture;
+    v_aColor    = aColor;
+    v_aTexture  = aTexture;
 }
